df58902b3a4c98605f00d47442a9ee88f7fc4d19
drago1979 <78018887+drago1979@users.noreply.github.com>
2022-01-24 23:50:04 +0100

    # File parsing command: basic setup done. # Command calls FileParsing service-method.

---
+++ app/Console/Commands/LoadSupplierProducts.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\CsvFileParser;
+use Illuminate\Console\Command;
+
+class LoadSupplierProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'loadsupplierproducts';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Loads suppliers and their products from CSV file';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @param CsvFileParser $fileParser
+     * @return int
+     */
+    public function handle(CsvFileParser $fileParser)
+    {
+        // Default path where the file should be stored
+        $filePath = storage_path(
+            'app' . DIRECTORY_SEPARATOR .
+            'file_parsing' . DIRECTORY_SEPARATOR .
+            'input_files' . DIRECTORY_SEPARATOR .
+            'suppliers_test.csv'
+        );
+
+        // We ask the user to choose between default and custom path
+        if ($this->confirm("### Do you want to use default path for input file ?\n\n ### Default path is:\n" . $filePath)) {
+            //
+        } else {
+            $filePath = $this->ask('### Please enter custom path.');
+        }
+
+        $fileParser->parseFile($filePath);
+
+        $this->info("\n### Command is executed.\nPlease check for results.");
+
+    }
+}
---
+++ app/Services/CsvFileParser.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Services;
+
+
+class CsvFileParser
+{
+
+    public function parseFile($filePath)
+    {
+        echo 'path is: ' . $filePath;
+    }
+}
